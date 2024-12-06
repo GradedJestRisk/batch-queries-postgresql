143c8cd12dbad4a4f2a72f9da47a1822ead35adf
loubna.hafidi <loubna.hafidi@octo.com>
2024-12-06 12:15:02 +0100

    add env file on windows

---
+++ sql/instance/setup.bat
@@ -0,0 +1,8 @@
+setx dbname database
+setx user user
+setx password password
+
+setx CONNECTION_STRING postgres://user:password@localhost:5432/database
+setx CONNECTION_STRING_ADMIN "host=localhost port=5432 dbname=database user=postgres password=admin_password application_name=batch-queries-postgresql-postgresql-1"
+
+setx CONNECTION_STRING_FLIGHT postgres://user:password@localhost:5432/flight
